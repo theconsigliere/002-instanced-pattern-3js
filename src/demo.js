cb24906502b0c5ea77fc97ddaee6bea03e5283c2
Dirty Martini <dirtymartini@maxwells-mbp-2.home>
2023-12-22 19:59:01 +0000

    22/12

--- src/demo.js
+++ src/demo.js
@@ -3,10 +3,10 @@ import { gsap } from "gsap"
 
 import { Rendering } from "./rendering"
 
-import * as THREE from "three";
+import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
-import { palettes, sinPalettes } from "./palettes";
+import { palettes, sinPalettes } from "./palettes"
 
 let paletteKey = "blue"
 let palette = palettes[paletteKey]
@@ -14,25 +14,62 @@ let sinPalette = sinPalettes[paletteKey]
 
 // setting up
 let rendering = new Rendering(document.querySelector("#canvas"), palette)
-rendering.camera.position.x = 80;
+rendering.camera.position.x = 80
 
 let controls = new OrbitControls(rendering.camera, rendering.canvas)
 
-let uTime = { value: 0 };
+let uTime = { value: 0 }
 
 // Init
 
-let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshNormalMaterial();
-let mesh = new THREE.Mesh(geometry, material);
+let radius = 2 / 3
+let rings = 1
+let segments = 32
+let totalInstances = rings * segments
+let geometry = new THREE.CylinderGeometry(radius, radius, 1, 8, 2)
+let instanceGeomtery = new THREE.InstancedBufferGeometry().copy(geometry)
+instanceGeomtery.instancedCount = totalInstances
+
+let vertexShader = glsl`
+  precision highp float;
+  uniform float uTime;
+  varying vec2 vUv;
+
+  void main() {
+    vec3 transformed = position;
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(transformed, 1.0);
+  }
+`
+
+let fragmentShader = glsl`
+  varying vec2 vUv;
+  uniform float uTime;
+
+  void main() {
+    vec3 color = vec3(0.);
+    color = vec3(vUv.y);
+    gl_FragColor = vec4(color, 1.0);
+  }
+`
+
+let material = new THREE.ShaderMaterial({
+  fragmentShader,
+  vertexShader,
+  uniforms: {
+    uTime,
+  },
+})
+
+let mesh = new THREE.Mesh(geometry, material)
+
 rendering.scene.add(mesh)
 
 // Events
 
-const tick = (t)=>{
-  uTime.value = t 
+const tick = (t) => {
+  uTime.value = t
   rendering.render()
 }
 
 gsap.ticker.add(tick)
-
